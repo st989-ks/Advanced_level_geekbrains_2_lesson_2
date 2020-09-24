cea00dc484d183a0dfaac9fc2a6ad9ec08052896
Stanislav <st989@ya.ru>
2020-09-24 22:35:25 +0400

    Initial commit

---
+++ src/taskForLesson_2/Main.java
@@ -0,0 +1,15 @@
+package taskForLesson_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[][] w={
+                {"1","2","3","4",},
+                {"5","6","7","8",},
+                {"9","10","11","12",},
+                {"13","14","15","16",},
+        };
+
+    }
+}
