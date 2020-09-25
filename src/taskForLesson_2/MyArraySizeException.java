985ff1aa940beaf8dc5666b8596452ddae0f2f08
Stanislav <st989@ya.ru>
2020-09-25 23:29:50 +0400

    Задание к уроку №2

--- src/taskForLesson_2/MyArraySizeException.java
+++ src/taskForLesson_2/MyArraySizeException.java
@@ -2,5 +2,11 @@ package taskForLesson_2;
 
 public class MyArraySizeException extends Exception {
 
+    MyArraySizeException() {
 
+    }
+
+    public void getMessageMySize() {
+        System.out.println("Массив не 4х4");
+    }
 }
