d917402e3e677579aa57d6087966cd227489f075
Stanislav <st989@ya.ru>
2020-09-25 23:26:45 +0400

    1

--- src/taskForLesson_2/Main.java
+++ src/taskForLesson_2/Main.java
@@ -2,14 +2,5 @@ package taskForLesson_2;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        String[][] w={
-                {"1","2","3","4",},
-                {"5","6","7","8",},
-                {"9","10","11","12",},
-                {"13","14","15","16",},
-        };
-
-    }
 }
